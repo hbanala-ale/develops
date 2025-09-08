fd5ee96d02f812484f553746e2200793a6b9b1b8
hbanala-ale <hbanala@avaya.com>
2025-09-08 15:24:17 +0530

    demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,9 @@
-class demo{
-  a=2,
-b=3,
-a+b,
-print(a+b)
+public class demo {
+    public static void main(String[] args) {
+        int num1 = 10; // first number
+        int num2 = 20; // second number
+
+        System.out.println("First number: " + num1);
+        System.out.println("Second number: " + num2);
+    }
 }
